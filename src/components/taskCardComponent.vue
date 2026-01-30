d39c79c3b771370c736d809abccdd634fcb5b9b3
Beto <kleberalbertocr@gmail.com>
2026-01-30 13:58:46 +0100

    Añado componente task

--- src/components/taskCardComponent.vue
+++ src/components/taskCardComponent.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-
+        <div v-for="(task,index) in tareas" :key="index">
+            {{ task }}
+        </div>
     </div>
 </template>
 
 <script setup>
-
+import { usePeticionStore } from '@/stores/peticion';
+import { onMounted, ref } from 'vue';
+const peticion = usePeticionStore()
+const tareas = ref([])
+onMounted(async()=>{
+    try {
+        const res = await peticion.getData()
+        tareas.value = res.data.todos
+    } catch (error) {
+        console.log(error)
+    }
+}
+)
 </script>
 
 <style lang="sass" scoped>
--- src/views/taskViewPage.vue
+++ src/views/taskViewPage.vue
@@ -1,27 +1,12 @@
 <template>
     <div>
         <h1>TASK</h1>
-        {{ tareas }}
-        <div v-for="(task,index) in tareas" :key="index">
-            {{ task }}
-        </div>
+        <taskCardComponent></taskCardComponent>
     </div>
 </template>
 
 <script setup>
-import { usePeticionStore } from '@/stores/peticion';
-import { onMounted, ref } from 'vue';
-const peticion = usePeticionStore()
-const tareas = ref([])
-onMounted(async()=>{
-    try {
-        const res = await peticion.getData()
-        tareas.value = res.data.todos
-    } catch (error) {
-        console.log(error)
-    }
-}
-)
+import taskCardComponent from '@/components/taskCardComponent.vue';
 
 
 </script>
