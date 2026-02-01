e877d9eb9fa9b76efbcb2b83d2e9d3a197245564
Veto con V <titobek21@gmail.com>
2026-02-01 22:28:10 +0100

    solucion un bug

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ const user = ref(null)
 
 onAuthStateChanged(auth,(firebaseUser)=>{
   user.value = firebaseUser
-
 })
 
 
@@ -16,7 +15,7 @@ onAuthStateChanged(auth,(firebaseUser)=>{
 <template>
   <header>
     <nav>
-      <div v-if="user" class="flex justify-between p-5"> 
+      <div v-if="user?.emailVerified" class="flex justify-between p-5"> 
         <router-link to="/" class="cursor-pointer transition-all bg-blue-500 text-white px-3 py-1 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">Mostrar tareas</router-link>
         <router-link to="/workspace" class="cursor-pointer transition-all bg-blue-500 text-white px-3 py-1 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">Mis tareas</router-link>
       </div>
