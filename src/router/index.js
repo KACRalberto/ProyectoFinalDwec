99d0d79ae56035f653cd26546e44076768696c9b
Veto con V <titobek21@gmail.com>
2026-02-01 17:23:08 +0100

    no me va deploy?

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/firebase/config'
+import WorkSpaceViewPage from '@/views/workspaceViewPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path:"/", component: ()=> import("@/views/taskViewPage.vue"), meta:{requiresLogged:true}},
-    {path: "/workspace", component: ()=> import("@/views/workspaceViewPage.vue"), meta:{requiresLogged:true}},
+    {path: "/workspace", component: WorkSpaceViewPage, meta:{requiresLogged:true}},
     {path : "/auth", component:()=> import("@/views/authViewPage.vue"), meta:{isAuth:true}}
   ],
 })
